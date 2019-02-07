fbe323fad5ef346c4a416b43a8a9e59d56333a51
alex <sslockk@mail.ru>
2019-02-07 10:25:43 +0300

    Lesson08, AlexGreen, sslockk@mail.ru

---
+++ Lesson08/L08_work_02.py
@@ -0,0 +1,41 @@
+# 2. Закодируйте любую строку из трех слов по алгоритму Хаффмана.
+
+import heapq
+from collections import Counter
+from collections import namedtuple
+
+
+class Node(namedtuple("Node", ["left", "right"])):
+    def walk(self, code, acc):
+        self.left.walk(code, acc + "0")
+        self.right.walk(code, acc + "1")
+
+
+class Leaf(namedtuple("Leaf", ["char"])):
+    def walk(self, code, acc):
+        code[self.char] = acc or "0"
+
+
+def huffman_code(s):
+    h = []
+    for ch, freq in Counter(s).items():
+        h.append((freq, len(h), Leaf(ch)))
+    heapq.heapify(h)
+    count = len(h)
+    while len(h) > 1:
+        freq1, _count1, left = heapq.heappop(h)
+        freq2, _count2, right = heapq.heappop(h)
+        heapq.heappush(h, (freq1 + freq2, count, Node(left, right)))
+        count += 1
+    code = {}
+    if h:
+        [(_freq, _count, root)] = h
+        root.walk(code, "")
+    return code
+
+
+s = input('введите строку из трех слов:')
+code = huffman_code(s)
+main_cod = "".join(code[ch] for ch in s)
+print('Таблица кодов', code)
+print('Код по алгоритму Хоффмана:\n', main_cod)
